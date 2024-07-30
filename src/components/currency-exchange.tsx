bde5a236bb02a7124a1f2eb376288735b203a5ae
fabri86 <ing.fabrizioiacovone@gmail.com>
2024-07-30 13:43:18 +0200

    Set default currencies on data loaded. Added error message when conversion fails

--- src/components/currency-exchange.tsx
+++ src/components/currency-exchange.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import clsx from 'clsx'
 
 import { useCurrencies } from '../hooks/use-currencies'
@@ -8,47 +8,53 @@ import { Currency } from '../types/currency.types'
 
 export const CurrencyExchange = () => {
   const { currencies, isLoading, error: loadingError } = useCurrencies()
-  const { convertCurrency, isProcessing, error, result } = useCurrencyConverter()
+  const { convertCurrency, isProcessing, error: conversionError, result } = useCurrencyConverter()
 
   const [fromAmount, setFromAmount] = useState<string>('')
   const [selectedFromCurrency, setSelectedFromCurrency] = useState<Currency | null>(null)
   const [selectedToCurrency, setSelectedToCurrency] = useState<Currency | null>(null)
 
-  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleAmountChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value as string
 
     setFromAmount(value)
-  }
+  }, [])
 
-  const handleCurrencyFromChanged = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = e.target.value
+  const handleCurrencyFromChanged = useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      const value = e.target.value
 
-    const selectedCurrency = currencies.find((c) => c.short_code === value)
+      const selectedCurrency = currencies.find((currency) => currency.short_code === value)
 
-    if (selectedCurrency) {
-      setSelectedFromCurrency(selectedCurrency)
-    }
-  }
+      if (selectedCurrency) {
+        setSelectedFromCurrency(selectedCurrency)
+      }
+    },
+    [currencies]
+  )
 
-  const handleCurrencyToChanged = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = e.target.value
+  const handleCurrencyToChanged = useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      const value = e.target.value
 
-    const selectedCurrency = currencies.find((c) => c.short_code === value)
+      const selectedCurrency = currencies.find((currency) => currency.short_code === value)
 
-    if (selectedCurrency) {
-      setSelectedToCurrency(selectedCurrency)
-    }
-  }
+      if (selectedCurrency) {
+        setSelectedToCurrency(selectedCurrency)
+      }
+    },
+    [currencies]
+  )
 
   const onCurrencyExchangeRun = () => {
     if (!selectedFromCurrency || !selectedToCurrency) {
-      return alert('Please select currencies')
+      return alert('Please select the desired currencies')
     }
 
     const parsedAmount = fromAmount ? Number.parseFloat(fromAmount) : ''
 
     if (!parsedAmount) {
-      return alert('Please insert a valid amount')
+      return alert('Please insert a valid numeric amount')
     }
 
     convertCurrency({
@@ -59,15 +65,16 @@ export const CurrencyExchange = () => {
   }
 
   useEffect(() => {
-    console.log({ result })
-  }, [result])
+    setSelectedFromCurrency(currencies?.[0])
+    setSelectedToCurrency(currencies?.[0])
+  }, [currencies])
 
   if (isLoading) {
-    return <p className="h-full text-xl text-white">Loading...</p>
+    return <p className="h-full text-xl text-white">Loading currencies...</p>
   }
 
   if (loadingError) {
-    return <p className="h-full text-xl text-red-400">Error: {error}</p>
+    return <p className="h-full text-base text-red-400">Error: {loadingError}</p>
   }
 
   return (
@@ -92,15 +99,25 @@ export const CurrencyExchange = () => {
         onSetCurrency={handleCurrencyToChanged}
       />
 
-      <button
-        className={clsx('w-24 mt-6 text-white bg-orange-400 rounded-md', {
-          'bg-gray-300': isProcessing,
-        })}
-        onClick={onCurrencyExchangeRun}
-        disabled={isProcessing}
-      >
-        {isProcessing ? 'Converting' : 'CONVERT'}
-      </button>
+      <div className="flex flex-col mt-2 gap-y-3">
+        {
+          <p className="h-2 text-xs text-red-400">
+            {conversionError
+              ? 'An error occurred while converting the currency. Please try again'
+              : ''}
+          </p>
+        }
+
+        <button
+          className={clsx('w-24 text-white bg-orange-400 rounded-md', {
+            'bg-gray-300': isProcessing,
+          })}
+          onClick={onCurrencyExchangeRun}
+          disabled={isProcessing}
+        >
+          {isProcessing ? 'Converting' : 'CONVERT'}
+        </button>
+      </div>
     </div>
   )
 }
